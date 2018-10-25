19e4940f271cfae37a0b236fda4777a6f7c6192d
Eric Peterson <eric@rigetti.com>
2018-10-24 20:15:33 -0400

    more lil lisp fixes

--- src-tests/test-rpc.lisp
+++ src-tests/test-rpc.lisp
@@ -5,70 +5,65 @@
 (in-package #:rpcq-tests)
 
 
+(defparameter *expected-response* "test-response")
+
+(defun test-method (&key (sleep 1 sleep-p))
+  (when sleep-p
+    (sleep sleep))
+  "test-response")
+
+
 (deftest test-client-server-dialogue ()
-  (let* ((expected-response "test-response")
-         (method-name "test-method")
-         (server-thread
+  (let* ((server-function
            (lambda ()
              (let ((dt (rpcq:make-dispatch-table)))
-               (rpcq:dispatch-table-add-handler dt (constantly expected-response)
-                                                :name method-name)
+               (rpcq:dispatch-table-add-handler dt 'test-method)
                (rpcq:start-server :dispatch-table dt
-                                  :listen-addresses '("inproc://RPCQ-test"))))))
-    ;; spawn the server thread
-    (let ((server-thread (bt:make-thread server-thread)))
-      (unwind-protect
-           (progn
-             (sleep 1)
-             ;; hook up the client
-             (rpcq:with-rpc-client (client "inproc://RPCQ-test")
-               ;; send a communique
-               (let ((server-response (rpcq:rpc-call client method-name)))
-                 (is (string= expected-response server-response)))))
-        ;; kill the server thread
-        (bt:destroy-thread server-thread)))))
+                                  :listen-addresses '("inproc://RPCQ-test")))))
+         (server-thread (bt:make-thread server-function)))
+    (sleep 1)
+    (unwind-protect
+      ;; hook up the client
+      (rpcq:with-rpc-client (client "inproc://RPCQ-test")
+        ;; send a communique
+        (let ((server-response (rpcq:rpc-call client "test-method")))
+          (is (string= *expected-response* server-response))))
+      ;; kill the server thread
+      (bt:destroy-thread server-thread))))
 
 (deftest test-client-timeout ()
-  (let* ((method-name "test-method")
-         (server-thread
+  (let* ((server-function
            (lambda ()
              (let ((dt (rpcq:make-dispatch-table)))
-               (rpcq:dispatch-table-add-handler dt (lambda () (sleep 5))
-                                                :name method-name)
+               (rpcq:dispatch-table-add-handler dt 'test-method)
                (rpcq:start-server :dispatch-table dt
-                                  :listen-addresses '("inproc://RPCQ-test"))))))
-    ;; spawn the server thread
-    (let ((server-thread (bt:make-thread server-thread)))
-      (unwind-protect
-           (progn
-             (sleep 1)
-             ;; hook up the client
-             (rpcq:with-rpc-client (client "inproc://RPCQ-test" :timeout 1)
+                                  :listen-addresses '("inproc://RPCQ-test")))))
+         (server-thread (bt:make-thread server-function)))
+    (sleep 1)
+    (unwind-protect
+         ;; hook up the client
+         (rpcq:with-rpc-client (client "inproc://RPCQ-test" :timeout 1)
                ;; send a communique
                (signals sb-ext:timeout
-                 (rpcq:rpc-call client method-name))))
-        ;; kill the server thread
-        (bt:destroy-thread server-thread)))))
+                 (rpcq:rpc-call client "test-method" :sleep 5)))
+      ;; kill the server thread
+      (bt:destroy-thread server-thread))))
 
 (deftest test-server-timeout ()
-  (let* ((method-name "test-method")
-         (server-thread
+  (let* ((server-function
            (lambda ()
              (let ((dt (rpcq:make-dispatch-table)))
-               (rpcq:dispatch-table-add-handler dt (lambda () (sleep 5))
-                                                :name method-name)
+               (rpcq:dispatch-table-add-handler dt 'test-method)
                (rpcq:start-server :timeout 1
                                   :dispatch-table dt
-                                  :listen-addresses '("inproc://RPCQ-test"))))))
-    ;; spawn the server thread
-    (let ((server-thread (bt:make-thread server-thread)))
-      (unwind-protect
-           (progn
-             (sleep 1)
-             ;; hook up the client
-             (rpcq:with-rpc-client (client "inproc://RPCQ-test")
+                                  :listen-addresses '("inproc://RPCQ-test")))))
+         (server-thread (bt:make-thread server-function)))
+    (sleep 1)
+    (unwind-protect
+         ;; hook up the client
+         (rpcq:with-rpc-client (client "inproc://RPCQ-test")
                ;; send a communique
                (signals rpcq::rpc-error
-                 (rpcq:rpc-call client method-name))))
-        ;; kill the server thread
-        (bt:destroy-thread server-thread)))))
+                 (rpcq:rpc-call client "test-method" :sleep 5)))
+      ;; kill the server thread
+      (bt:destroy-thread server-thread))))
--- src/client.lisp
+++ src/client.lisp
@@ -65,18 +65,23 @@
 
 
 (defun prepare-rpc-call-args (args)
-  (let ((**kwargs (make-hash-table :test #'equal)))
-    (loop :for arg :in args
-          :for rest := args :then (rest rest)
-          :if (typep arg 'keyword)
-            :return  (progn
-                       (loop :for (key val) :on rest :by #'cddr :while val
-                             :do (setf (gethash (sanitize-name key) hash-table) val))
-                       (alexandria:plist-hash-table (list "*args" *args
-                                                          "**kwargs" **kwargs)
-                                                    :test #'equal))
-          :else
-            :collect arg :into *args)))
+  (cond
+    ((null args)
+     (alexandria:plist-hash-table (list "*args" '()
+                                        "**kwargs" (make-hash-table :test #'equal))))
+    (t
+     (let ((**kwargs (make-hash-table :test #'equal)))
+       (loop :for arg :in args
+             :for rest := args :then (rest rest)
+             :if (keywordp arg)
+               :return (progn
+                         (loop :for (key val) :on rest :by #'cddr :while val
+                               :do (setf (gethash (sanitize-name key) **kwargs) val))
+                         (alexandria:plist-hash-table (list "*args" *args
+                                                            "**kwargs" **kwargs)
+                                                      :test #'equal))
+             :else
+               :collect arg :into *args)))))
 
 (defun rpc-call (client call &rest args)
   "Makes a synchronous RPC call, designated by the string method name CALL, over the connection CLIENT.  ARGS is a plist of arguments.  Returns the result of the call directly."
