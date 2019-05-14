12703ecfdc4fa7d8b69a39408877c9e9431187ab
Prasahnt Sivarajah <prasahnt@rigetti.com>
2019-05-14 16:04:46 -0700

    :sparkles: phase and detuning features in Capture message.

--- rpcq/core_messages.py
+++ rpcq/core_messages.py
@@ -542,6 +542,14 @@ class Capture(Instruction):
     """Transmit the readout bit back to Lodgepole. 
           (Unnecessary for fully calibrated active reset captures)."""
 
+    phase: float = 0.0e+0
+    """Static phase angle [units of tau=2pi] by which the 
+          envelope quadratures are rotated."""
+
+    detuning: float = 0.0e+0
+    """Detuning [Hz] with which the pulse envelope should be 
+          modulated relative to the frame frequency."""
+
 
 @dataclass(eq=False, repr=False)
 class RackMeta(Message):
