989788d243a381bad899e9d463158fe31137c544
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-05-17 18:09:23 -0700

    Replace - with _ when generating python modules

--- update_python_spec.lisp
+++ update_python_spec.lisp
@@ -1,13 +1,15 @@
 #!/usr/bin/env sbcl --script
 (load "~/.sbclrc")
 (ql:quickload :rpcq)
+(ql:quickload :split-sequence)
 (rpcq::clear-messages)
 
 (dolist (namespace '("messages" "core-messages"))
   (load (make-pathname :name namespace :type "lisp" :directory '(:relative "src")))
-  (with-open-file (f (make-pathname :name namespace :type "py" :directory '(:relative "rpcq"))
+  (let ((path (make-pathname :name (rpcq::sanitize-name namespace) :type "py" :directory '(:relative "rpcq"))))
+    (with-open-file (f path
                      :direction ':output
                      :if-exists ':supersede
                      :if-does-not-exist ':create)
-    (rpcq::python-message-spec f (gethash namespace rpcq::*messages*))
-    (format t "Wrote new ~A.py~%" namespace)))
+      (rpcq::python-message-spec f (gethash namespace rpcq::*messages*))
+      (format t "Wrote new ~A.py~%" (pathname-name path)))))
